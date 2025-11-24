f89d9c510067295a0f49c05e3a8cef30524cb709
BRAHADEESWARAR B <106338446+0XBrahadeeswarar@users.noreply.github.com>
2025-11-24 10:52:07 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,1 @@
-def add_numbers(a, b):
-    """
-    Returns the sum of two numbers.
-    Raises:
-        TypeError: If inputs are not int or float.
-    """
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("Both arguments must be int or float")
-    return a + b
 print("Hello World!")
